c2899cab4f5f51cf9d29f0fc5233a50055ad23c7
Marjolein Knijff <m.knijff@student.tudelft.nl>
2019-03-28 20:57:07 +0100

    Added feature extraction for Bag of words and question

--- features.py
+++ features.py
@@ -1,3 +1,7 @@
+import numpy as np
+import re
+
+
 #BoW marjolein
 #Q marjolein
 #RootDist Zhang
@@ -5,3 +9,28 @@
 #PPDB Priya
 #SVO Priya
 #word2vec Priya
+
+def BoW (claim, headline):
+
+    wordsClaim = re.sub("[^\w]", " ", claim).split()
+    wordsClaim_cleaned = [w.lower() for w in wordsClaim]
+    wordsClaim_cleaned = sorted(list(set(wordsClaim_cleaned)))
+
+    wordsHeadline = re.sub("[^\w]", " ", headline).split()
+    wordsHeadline_cleaned = [w.lower() for w in wordsHeadline]
+    wordsHeadline_cleaned = sorted(list(set(wordsHeadline_cleaned)))
+
+    bag = np.zeros(len(wordsClaim_cleaned))
+    for hw in wordsHeadline_cleaned:
+        for i, cw in enumerate(wordsClaim_cleaned):
+            if hw == cw:
+                bag[i] += 1
+
+    return np.array(bag)
+
+
+def Q (claim, headline):
+    if "?" in headline:
+        return 1
+    else:
+        return 0
