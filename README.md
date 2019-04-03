95e85cdf144b569a0b166cd35df465c477f4faca
priyasarkar <priyasarkar.contact@gmail.com>
2019-04-03 17:10:44 +0200

    .gitignore is now working

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# Create the environment
-conda create -n nlpGroup python=3.7.1
-conda activate nlpGroup
-
-#Set up environment and install packages
-conda install -c anaconda nltk
-pip install scikit-multilearn
-pip install vaderSentiment
+version https://git-lfs.github.com/spec/v1
+oid sha256:41b42cf217aacf860681b9f993fa635691fc0acf66e12d4b1c87866f19d3c3c9
+size 217
--- utils.py
+++ utils.py
@@ -1,36 +1,3 @@
-import numpy as np
-import re
-
-from nltk import stem
-from logger import Logger
-
-
-MIN_ALIGNMENT_SCORE = -10
-MAX_ALIGNMENT_SCORE = 10
-
-def cosine_similarity_by_vector(vector1, vector2):
-    return np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
-
-def alignment_score(file, word1, word2):
-    stemmer = stem.SnowballStemmer("english")
-    if(stemmer.stem(word1) == stemmer.stem(word2)):
-        return MAX_ALIGNMENT_SCORE
-    else:
-        return _paraphrase_score(file, word1, word2)
-
-
-def _paraphrase_score(file, word1, word2):
-    paraphrase_score = MIN_ALIGNMENT_SCORE
-    with open(file, 'r') as ppdb_lex:
-        lines = ppdb_lex.readlines()
-        matches = re.findall(word1,lines)
-        if matches:
-            final_matches = re.findall(word2,matches)
-            if final_matches:
-                for line in final_matches:
-                    match_build = line.split("|||")
-                    if ((match_build[1] == word1 & match_build[2] == word2) | (match_build[1] == word2 & match_build[2] == word1)):
-                        metrics = [metric for metric in match_build[3].split(" ")]
-                        scorings = {key_val[0]: int(key_val[1]) for key_val in [scoring.split("=") for scoring in metrics]}
-                        paraphrase_score = -np.log(scorings['p(f|e)']) -np.log(scorings['p(f|e)']) - np.log(scorings['p(e|f,LHS)']) - np.log(scorings['p(f|e,LHS)']) + 0.3 * (-np.log(scorings['p(LHS|e)'])) + 0.3 * (-np.log(scorings['p(LHS|f)'])) + 100 * scorings['RarityPenalty']
-    return paraphrase_score
+version https://git-lfs.github.com/spec/v1
+oid sha256:29232ece7076bee782e204d6c308f2053e7546cdac63915566023ce36fb4caa2
+size 1561
